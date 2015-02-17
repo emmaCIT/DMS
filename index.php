558c47850e7798399b4fb7bf78b83d802ae08aaa
EmmaCIT <emmaogbene@gmial.com>
2015-02-17 12:52:05 +0000

    The index page for login.

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include 'core/init.php';
+include 'includes/overall/header.php';
+?>
+
+<h1>Welcome</h1>
+<p>to the Diabetes Management System.</p>
+
+<?php include 'includes/overall/footer.php';?>
+
