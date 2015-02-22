a60a2e766c2e799a23d94d108b6ff19469130235
EmmaCIT <emmaogbene@gmial.com>
2015-02-22 00:22:13 +0000

    #Changed the look of the home page.

--- patientData.php
+++ patientData.php
@@ -4,8 +4,8 @@ include 'patientInterface/includes/overall/header.php';
 ?>	          
         
 		<section>
-			<h1>You have logged in as a Patient</h1>
-			<p>into the Diabetes Management System.</p>
+			<h1>Home</h1>
+			<p>I am a Patient</p>
 		</section>
 
 <?php include 'patientInterface/includes/overall/footer.php'; ?>
