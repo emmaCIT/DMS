ab0eb3216f5f8869bdcf1acaf683713f3a766040
EmmaCIT <emmaogbene@gmial.com>
2015-02-22 01:30:39 +0000

    Error handling for changepassword.php

--- changepassword.php
+++ changepassword.php
@@ -1,5 +1,16 @@
 <?php
 include 'core/init.php';
+
+if(empty($_POST) === false) {
+	$required_fields = array('current_password', 'password', 'password_again');
+	foreach($_POST as $key=>$value) {
+		if(empty($value) && in_array($key, $required_fields) === true) {
+			$errors[] = 'Fields marked with an asterisk are required';
+			break 1;
+				
+		}
+	}
+}
 include 'patientInterface/includes/overall/header.php';
 ?>
 
@@ -16,6 +27,9 @@ include 'patientInterface/includes/overall/header.php';
 		</li>
 		<li>New password again*: <br> 
 			<input type="text" name="password_again">
+		</li>
+		<li> 
+			<input type="submit" value="Change password">
 		</li>
 	</ul>
 </form>
