619d22bf998a1306e00f79ced319b72731f0369e
EmmaCIT <emmaogbene@gmial.com>
2015-02-22 00:18:52 +0000

    Changes were made to the patientData.php

--- patientData.php
+++ patientData.php
@@ -4,8 +4,8 @@ include 'patientInterface/includes/overall/header.php';
 ?>	          
         
 		<section>
-			<h1>Welcome</h1>
-			<p>to the Diabetes Management System.</p>
+			<h1>You have logged in as a Patient</h1>
+			<p>into the Diabetes Management System.</p>
 		</section>
 
 <?php include 'patientInterface/includes/overall/footer.php'; ?>
