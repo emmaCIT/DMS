72be25690cd9b7fad10051bdb9cfbfbb6de2492f
EmmaCIT <emmaogbene@gmial.com>
2015-02-22 00:30:14 +0000

    The following pages were modified.

--- statistics.php
+++ statistics.php
@@ -1,1 +1,14 @@
 <?php
+include 'core/init.php';
+include 'patientInterface/includes/overall/header.php';
+?>	          
+        
+		<section>
+			<h1>Statistics Record</h1>
+			
+		</section>
+
+<?php include 'patientInterface/includes/overall/footer.php'; ?>
+
+
+
