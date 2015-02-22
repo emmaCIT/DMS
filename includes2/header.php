e5abf69e94068a4d83bd69195917c368690c7468
EmmaCIT <emmaogbene@gmial.com>
2015-02-22 00:04:31 +0000

    Deleted the pages

--- includes2/header.php
+++
@@ -1,5 +0,0 @@
-<header>
-	<img alt="" src="image/logo.jpg">
-	<?php include 'patientInterface/includes/menu.php';?>
-	<div class="clear"></div>
-</header>
--- includes2/menu.php
+++
@@ -1,8 +0,0 @@
-<nav>
-	<ul>
-		<li><a href="profile.php">Profile</a></li>
-		<li><a href="record.php">Record Details</a></li>
-		<li><a href="notes.php">Personal Notes</a></li>
-		<li><a href="statistics.php">Statistics</a></li>
-	</ul>
-</nav>
