b8ab0a62c456cedb71528b25fa54a74d4e835063
EmmaCIT <emmaogbene@gmial.com>
2015-02-23 20:40:16 +0000

    Changing passwords

--- changepassword.php
+++ changepassword.php
@@ -11,21 +11,33 @@ if(empty($_POST) === false) {
 	}
 	
 	if (md5($_POST['current_password']) === $user_data['password']) {
-		if ($_POST['password'] === $_POST['password_again']) {
-			
+		if (trim($_POST['password']) !== trim($_POST['password_again'])) {
+			$errors[] = 'Your new passwords do not match';
+		}else if (strlen($_POST['password']) < 8) {
+			$errors[] = 'Your password must be at least 8 characters';
 		}
 	} else {
 		$errors[] = 'Your current password is incorrect';
 	}
-	print_r($errors);
 }
 
 include 'patientInterface/includes/overall/header.php';
+
 ?>
 
 <section>
 	<h1>Change Password</h1>
 
+<?php 
+if (empty($_POST) === false && empty($errors) === true) {
+	//posted the form and no errors.
+	change_password();
+}else if (empty($errors) === false) {
+	//output errors
+	echo output_errors($errors);
+}
+
+?>
 <form action="" method="post">
 	<ul>
 		<li>Current password*: <br> 
