7729d2ccc843b39e604c7b2c7093c9643bcf5ed4
EmmaCIT <emmaogbene@gmial.com>
2015-02-24 02:38:18 +0000

    changed made for email activation.

--- core/functions/patients.php
+++ core/functions/patients.php
@@ -14,7 +14,7 @@ function register_user($register_data) {
 	$data = '\'' . implode('\', \'', $register_data) . '\'';
 
 	mysql_query("INSERT INTO `users` ($fields) VALUES ($data)");
-	email($register_data['email'], 'Activate your account', "Hello " . $register_data['first_name'] . ", \n\nYou need to activate your account, therefore use the link below:\n\nhttp://localhost/dms/activate.php?email=" . $register_data['email'] . "&email_code=" . $register_data['email_code'] . "\n\n- DMS");
+	email($register_data['email'], 'Activate your account', "Hello " . $register_data['first_name'] . ", \n\nYou need to activate your account, therefore use the link below:\n\nhttp://localhost/dms/activate.php?email=" . $register_data['email'] . "&email_code=" . $register_data['email_code'] . "\n\n- Diabetes Management System");
 }
 
 
