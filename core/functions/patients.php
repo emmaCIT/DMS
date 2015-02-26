c674b71c8ae86667c61c43801466f0165cc3482f
EmmaCIT <emmaogbene@gmial.com>
2015-02-26 00:28:07 +0000

    #User Settings tab completed. #Information added to the Documentation

--- core/functions/patients.php
+++ core/functions/patients.php
@@ -1,4 +1,15 @@
 <?php
+function update_user($update_data) {
+	global $session_user_id;
+	$update = array();
+	array_walk($update_data, 'array_sanitize');
+	
+	foreach ($update_data as $field=>$data) {
+		$update[] = '`' . $field . '` = \'' . $data . '\'';
+	}
+	mysql_query("UPDATE `users` SET " . implode(', ', $update) . " WHERE `user_id` = $session_user_id");
+}
+
 function activate($email, $email_code) {
 	$email 		=mysql_real_escape_string($email);
 	$email_code =mysql_real_escape_string($email_code);
