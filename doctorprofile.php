e96622231b244f39cf7f97d49ea0719db271bb7f
EmmaCIT <emmaogbene@gmial.com>
2015-02-28 21:47:28 +0000

    Doctor's profile being created

---
+++ doctorprofile.php
@@ -0,0 +1,13 @@
+<?php
+include 'core/init.php';
+protect_page();
+include 'doctorInterface/includes/overall/header.php';
+?>	          
+
+			<h1>This is the Administrator's page.</h1>
+
+
+<?php include 'doctorInterface/includes/overall/footer.php'; ?>
+
+
+
