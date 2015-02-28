9c8e02ac0115b9cbaefc77f94399e13291a93ba7
EmmaCIT <emmaogbene@gmial.com>
2015-02-28 14:27:20 +0000

    Recovering User's username and password.

--- includes/widgets/login.php
+++ includes/widgets/login.php
@@ -9,6 +9,7 @@
 				</li>
 				<li><input type="submit" value="Log in"></li>
 				<li><a href ="register.php">Register</a></li>
+				<li>Forgot your <a href ="recover.php?mode=username">username </a>or <a href ="recover.php?mode=password">password?</a></li>
 			</ul>
 		</form>
 	</div>
